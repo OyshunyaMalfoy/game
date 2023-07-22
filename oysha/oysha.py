6491ca9a239d919673c744ade626f551b7edb85f
OyshunyaMalfoy <aisha13092009@gmail.com>
2023-07-22 21:01:46 +0500

    1234

--- oysha/oysha.py
+++ oysha/oysha.py
@@ -3,9 +3,11 @@
 # print(b)
 
 
-a = list(range(3,101,3))
-print(sum(a))
+# a = list(range(3,101,3))
+# print(sum(a))
 
 
-
-
+a = [int(input()),int(input())]
+b = min(a)
+c = list(range(1,b+1))
+print(sum(c))
